694fe32ef8387585e7cbf2a8108b45933ed0df9c
Vitor Diorio <vdioriomd@gmail.com>
2026-05-21 22:30:49 -0300

    feat(config): add @/* path alias and reanimated babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-worklets/plugin', // MUST be last (Reanimated 4 uses worklets plugin)
+  ],
 };
